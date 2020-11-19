da35c3264b55d323c7fbe2659e96fe3d35b33cbe
taehun <asdnm5030@naver.com>
2020-11-19 12:11:21 +0900

    메뉴 통합 작업

--- src/main/java/com/example/study/service/SideMenuService.java
+++ src/main/java/com/example/study/service/SideMenuService.java
@@ -24,9 +24,9 @@ public class SideMenuService {
                 SideMenu.builder().title("계약서 종류 관리").url("/pages/contract/contractType").mode("item").build().check(title),
                 SideMenu.builder().title("부서/팀 관리").url("/pages/department").mode("item").build().check(title),
                 SideMenu.builder().title("직위 관리").url("/pages/rank").mode("item").build().check(title),
-				SideMenu.builder().title("계약서 권한 관리1").url("/pages/contract").mode("item").build().check(title),
+				SideMenu.builder().title("계약서 권한 관리1").url("/pages/auth").mode("item").build().check(title),
                 SideMenu.builder().title("계약서 권한 관리2").url("/pages/authuser").mode("item").build().check(title),
-                SideMenu.builder().title("권한 문의").url("/pages/authInquire").mode("item").build().check(title)
+                SideMenu.builder().title("권한 문의").url("/pages/authInquire").mode("item").build().check(title),
                 
                 SideMenu.builder().title("TEST").mode("header").build(),
                 SideMenu.builder().title("계약관리").url("/pages/contract").mode("item").build().check(title),
